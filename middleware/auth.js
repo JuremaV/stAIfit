cbb7b68fc5616382bba06ace52a480b648aeebe3
Jurema Vidal <juremavidal.jv62@gmail.com>
2024-10-27 20:51:01 +0200

    Updated feature or bug fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,24 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-  const authHeader = req.header('Authorization');
-  const token = authHeader && authHeader.split(' ')[1]; // Extract token from 'Bearer <token>'
+  const authHeader = req.header("Authorization");
+  // Extract token from 'Bearer <token>'
+  const token = authHeader && authHeader.split(" ")[1];
   if (!token) {
-      return res.status(401).json({ msg: 'No token, authorization denied' });
-  }  
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Using JWT_SECRET from .env
+    // Using JWT_SECRET from .env
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
     console.log("Decoded user from token:", decoded.user);
     next();
   } catch (err) {
     console.error("Token verification error:", err);
-    res.status(401).json({ msg: 'Token is not valid' });
+    res.status(401).json({ msg: "Token is not valid" });
   }
 }
 
 module.exports = auth;
-
 
